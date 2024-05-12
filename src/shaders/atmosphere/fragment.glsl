7f23de91da9b711004880e48ce877bba9c3c1af8
alessandrobattiato <alessandroDev@outlook.it>
2024-05-12 19:22:09 +0200

    feat: implemented external atmosphere

--- src/shaders/atmosphere/fragment.glsl
+++ src/shaders/atmosphere/fragment.glsl
@@ -17,10 +17,18 @@ void main()
     // Atmosphere
     float atmosphereDayMix = smoothstep(- 0.5, 1.0, sunOrientation);
     vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayMix);
-    color = mix(color, atmosphereColor, atmosphereDayMix);
+    color += atmosphereColor;
+
+    // Alpha
+    float edgeAlpha = dot(viewDirection, normal);
+    edgeAlpha = smoothstep(0.0, 0.5, edgeAlpha);
+
+    float dayAlpha = smoothstep(- 0.5, 0.0, sunOrientation);
+
+    float alpha = edgeAlpha * dayAlpha; // this fixes the external atmosphere fog to be weaker on the dark side and brighter towards the sun
 
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
