512d10f86019fd2790c2a88bf6801d064efbacd3
alessandrobattiato <alessandroDev@outlook.it>
2024-05-12 18:24:49 +0200

    feat: atmosphere Color blend of day and night implemented

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -2,6 +2,8 @@ uniform sampler2D uDayTexture;
 uniform sampler2D uNightTexture;
 uniform sampler2D uSpecularCloudsTexture;
 uniform vec3 uSunDirection;
+uniform vec3 uAtmosphereDayColor;
+uniform vec3 uAtmosphereTwilightColor;
 
 varying vec2 vUv;
 varying vec3 vNormal;
@@ -30,6 +32,11 @@ void main()
     float cloudsMix = smoothstep(0.3, 1.0, specularCloudsColor.g); // The first parameter basically controls how many clouds should be seen on the earth, on the technical side, we are stating how much of the data texture which contains the clouds we want to take and use
     cloudsMix *= dayMix; // this makes clouds disappear on the dark side so you can clearly see the city lights without needing to darken the clouds which would have occluded the city lights as well
     color = mix(color, vec3(1.0), cloudsMix);
+
+    // Atmosphere
+    float atmosphereDayMix = smoothstep(- 0.5, 1.0, sunOrientation);
+    vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayMix);
+    color = atmosphereColor; // when it's toward the sun, we get the blue color, otherwise we get orange on the dark side
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
