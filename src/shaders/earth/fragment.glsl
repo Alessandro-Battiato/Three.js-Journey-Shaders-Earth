2f34fe824e006b74b045aa21e1e36236e1a53581
alessandrobattiato <alessandroDev@outlook.it>
2024-05-12 18:12:34 +0200

    feat: clouds implemented

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -23,6 +23,13 @@ void main()
     vec3 nightColor = texture(uNightTexture, vUv).rgb;
     color = mix(nightColor, dayColor, dayMix); // if value is 1 then we get the day, otherwise we get the night
 
+    // Specular clouds color
+    vec2 specularCloudsColor = texture(uSpecularCloudsTexture, vUv).rg;
+
+    // Clouds
+    float cloudsMix = smoothstep(0.3, 1.0, specularCloudsColor.g); // The first parameter basically controls how many clouds should be seen on the earth, on the technical side, we are stating how much of the data texture which contains the clouds we want to take and use
+    color = mix(color, vec3(1.0), cloudsMix);
+
     // Final color
     gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
